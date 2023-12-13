e8b4deeb4b25ff224357678e29856f7f3b3c5e63
Otunbanana <87811466+Otunbanana@users.noreply.github.com>
2023-12-13 23:01:35 +0100

    now

--- prompt.c
+++ prompt.c
@@ -12,15 +12,16 @@ void read_command(char *line)
 char *prompt = "#cisfun$ ";
 int i;
 
-for (i = 0; prompt[i] != '\0'; i++)
-{
-_putchar(prompt[i]);
-}
-if (fgets(line, MAX_LINE, stdin) == NULL)
-{
-_putchar('\n');
-exit(0);
-}
+if (isatty(STDIN_FILENO)) {
+        for (i = 0; prompt[i] != '\0'; i++) {
+            _putchar(prompt[i]);
+        }
+    }
 
-line[strlen(line) - 1] = '\0';
+    if (fgets(line, MAX_LINE, stdin) == NULL) {
+        _putchar('\n');
+        exit(0);
+    }
+
+    line[strlen(line) - 1] = '\0';
 }
